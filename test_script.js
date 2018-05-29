c54c499c291e78de0b841abb90e85fb053a301ec
Rafael Barreto <rafaelgavabarreto@gmail.com>
2018-05-29 17:41:00 +0000

    Fix the return to user

--- test_script.js
+++ test_script.js
@@ -2,12 +2,12 @@ const pg = require("pg");
 const settings = require("./settings"); // settings.json
 
 const client = new pg.Client({
-  user     : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
+  user: settings.user,
+  password: settings.password,
+  database: settings.database,
+  host: settings.hostname,
+  port: settings.port,
+  ssl: settings.ssl
 });
 
 client.connect((err) => {
@@ -16,14 +16,17 @@ client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
-  // client.query("SELECT $1::int AS number", ["1"], (err, result) => {
-    client.query(`SELECT * FROM famous_people WHERE first_name = $1 `, [someId], (err, result) => {
+  client.query(`SELECT * FROM famous_people WHERE first_name = $1 OR last_name = $1`, [someId], (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
-    // console.log(result.rows[0].number); //output: 1
-    console.log(result.rows);
+
+    let results = result.rows
+    console.log("Found " + result.rows.length + " person(s) by the name '" + someId + "':");
+    for (let i = 0; i < result.rows.length; i++) {
+      console.log("- ", (i + 1), ":", results[i].first_name, results[i].last_name, "born '" + results[i].birthdate.toLocaleDateString() + "'");
+    }
+
     client.end();
   });
 });
-
